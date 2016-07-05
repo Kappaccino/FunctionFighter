dfa467a8a0f5b869f0f20645c4e2336303aa72cb
RarCeth <rarceth@gmail.com>
2016-07-05 19:14:01 +0800

    Everything is awesome
    look at my horse, my horse is amazing.
    Give it a lick, it tastes just like raisins.

---
+++ Assets/Projectile_Script.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile_Script : MonoBehaviour {
+	public float a, b, c, t;
+	// Use this for initialization
+	void Start () {
+		t = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		t += Time.deltaTime;
+		FireAtWill (a, b, c, t);
+	}
+
+	public void FireAtWill (float a, float b, float c, float t){
+		transform.position = new Vector3 (t, (a*Mathf.Pow(t,2)) + (b*t) + c, 0);
+	}
+}
---
+++ Assets/Tank_Script.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tank_Script : MonoBehaviour {
+
+	public Vector3 targetPos;
+	public GameObject target;
+	public GameObject projectileTemplate;
+
+	public float a, b, c;
+
+	//y = mx^2 + c
+	// Use this for initialization
+	void Start () {
+		targetPos = target.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (targetPos);
+		if (Input.GetKeyDown(KeyCode.Space)){
+			FireProjectile ();
+		}
+	}
+
+	private void FireProjectile(){
+		GameObject projectile = Instantiate (projectileTemplate, transform.position, Quaternion.identity) as GameObject;
+		Projectile_Script p = projectile.GetComponent<Projectile_Script> ();
+		p.a = a;
+		p.b = b;
+		p.c = c;
+	}
+}
